f5829e22e989434332997b89ffb37c3d1df35025
Birdman1104 <arsenmazmanyan11@gmail.com>
2024-10-22 02:03:09 +0400

    Added main guy body.

---
+++ src/views/Characters/MainBody.ts
@@ -0,0 +1,119 @@
+import anime from 'animejs';
+import { Container, Sprite } from 'pixi.js';
+import { Images } from '../../assets';
+import { makeSprite } from '../../utils';
+import { MainHead } from './MainHead';
+
+export class MainBody extends Container {
+    private torso: Sprite;
+    private leftArm: Sprite;
+    private rightArm: Sprite;
+    private leftLeg: Sprite;
+    private rightLeg: Sprite;
+    private head: MainHead;
+
+    constructor() {
+        super();
+        this.build();
+    }
+
+    public happy(): void {
+        this.removeAnimations();
+
+        this.head.happy();
+
+        anime({
+            targets: this.rightArm,
+            angle: [0, -15],
+            duration: 800,
+            direction: 'alternate',
+            easing: 'easeInOutSine',
+            loop: true,
+        });
+    }
+
+    public idle(): void {
+        this.removeAnimations();
+
+        this.head.idle();
+
+        anime({
+            targets: this.rightArm,
+            angle: [0, -5],
+            duration: 1000,
+            direction: 'alternate',
+            easing: 'easeInOutSine',
+            loop: true,
+        });
+
+        anime({
+            targets: this.leftArm,
+            angle: [-2, 2],
+            duration: 2000,
+            direction: 'alternate',
+            easing: 'easeInOutSine',
+            loop: true,
+        });
+
+        anime({
+            targets: this.torso.scale,
+            x: 1.05,
+            duration: 2000,
+            direction: 'alternate',
+            easing: 'easeInOutSine',
+            loop: true,
+        });
+
+        anime({
+            targets: this.torso,
+            angle: 1,
+            duration: 1000,
+            direction: 'alternate',
+            easing: 'easeInOutSine',
+            loop: true,
+        });
+    }
+
+    private build(): void {
+        this.head = new MainHead();
+        this.addChild(this.head);
+
+        this.torso = makeSprite({ texture: Images['main_guy/torso'] });
+        this.addChild(this.torso);
+
+        this.leftArm = makeSprite({ texture: Images['main_guy/left_arm'] });
+        this.leftArm.anchor.set(0.6, 0.28);
+        this.leftArm.position.set(this.leftArm.width * 0.1, -this.leftArm.height * 0.22);
+        this.addChild(this.leftArm);
+
+        this.leftLeg = makeSprite({ texture: Images['main_guy/left_leg'] });
+        this.addChild(this.leftLeg);
+
+        this.rightLeg = makeSprite({ texture: Images['main_guy/right_leg'] });
+        this.addChild(this.rightLeg);
+
+        this.addChild(this.torso);
+
+        this.rightArm = makeSprite({ texture: Images['main_guy/right_arm'] });
+        this.rightArm.anchor.set(0.3, 0.21);
+        this.rightArm.position.set(-this.rightArm.width * 0.2, -this.rightArm.height * 0.29);
+        this.addChild(this.rightArm);
+    }
+
+    private removeAnimations(): void {
+        [
+            this,
+            this.torso,
+            this.rightArm,
+            this.leftArm,
+            this.rightLeg,
+            this.leftLeg,
+            this.scale,
+            this.torso.scale,
+            this.rightArm.scale,
+            this.leftArm.scale,
+            this.rightLeg.scale,
+            this.leftLeg.scale,
+        ].forEach((c) => anime.remove(c));
+    }
+}
--- src/views/Characters/MainGuy.ts
+++ src/views/Characters/MainGuy.ts
@@ -1,9 +1,10 @@
+import anime from 'animejs';
 import { Container } from 'pixi.js';
-import { MainHead } from './MainHead';
+import { delayRunnable } from '../../utils';
+import { MainBody } from './MainBody';
 
 export class MainGuy extends Container {
-    private head: MainHead;
-
+    private body: MainBody;
     constructor() {
         super();
 
@@ -11,12 +12,42 @@ export class MainGuy extends Container {
     }
 
     public idle(): void {
-        console.log('idle');
+        this.removeAnimations();
+        this.body.idle();
+        this.breathe();
+    }
+
+    public happy(): void {
+        this.removeAnimations();
+        this.body.happy();
+        this.breathe();
     }
 
     private build(): void {
-        this.head = new MainHead();
-        this.head.happy();
-        this.addChild(this.head);
+        this.body = new MainBody();
+        this.addChild(this.body);
+
+        this.idle();
+        delayRunnable(4, () => this.happy());
+    }
+
+    private breathe(): void {
+        anime({
+            targets: this.body.scale,
+            y: 1.015,
+            duration: 2000,
+            direction: 'alternate',
+            easing: 'easeInOutSine',
+            loop: true,
+            update: () => {
+                this.body.position.y = -((this.body.scale.y - 1) / 2) * this.body.height;
+            },
+        });
+    }
+
+    private removeAnimations(): void {
+        anime.remove(this.body.scale);
+        this.body.scale.set(1);
+        this.body.position.y = 0;
     }
 }
